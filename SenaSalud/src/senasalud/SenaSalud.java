5fa6cee74313e6529e70a3cc53429443ccce9a79
KarenOspina <karensofia@192.168.1.16>
2019-11-03 21:46:58 -0500

    mi primer comit

---
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package senasalud;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class SenaSalud {
+
+    
+    public static void main(String[] args) {
+    String nom,col,ape;
+    int ed;
+    Scanner nombre = new Scanner (System.in);
+    System.out.println("Digite su Nombre");
+    nom=nombre.next();
+    
+   
+    
+    System.out.println("Su nombre es:"+nom);
+   
+    }
+    
+}
